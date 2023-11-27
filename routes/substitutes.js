09227bd0879d0319ef7362907e7620e15a3358ae
MarkusIngerslev <markusolsen2004@gmail.com>
2023-11-27 15:48:28 +0100

    Tilføjet mulighed for at oprette ny person, i personale, samt mulighed for at hente enkel vikar ud fra id.

--- routes/substitutes.js
+++ routes/substitutes.js
@@ -4,7 +4,7 @@ import connection from "../database.js";
 
 const substitutesRouter = Router();
 
-// ===== SUBSTITUTE ROUTES ===== \\
+// ===== GET ALL SUBSTITUTES ===== \\
 substitutesRouter.get("/", (req, res) => {
     let queryString = ``;
 
@@ -17,10 +17,61 @@ substitutesRouter.get("/", (req, res) => {
             console.log(err);
             res.status(500).json({ error: "der opstod en fejl ved forespørgslen!" });
         } else {
-            console.log(results);
+            // console.log(results);
             res.json(results);
         }
     });
+});
+
+// ===== GET SINGLE SUBSTITUTE FROM ID ===== \\
+substitutesRouter.get("/:id", (req, res) => {
+    let queryString = ``;
+
+    queryString = /*sql*/ `
+        SELECT * FROM substitutes WHERE id = ?
+    `;
+
+    connection.query(queryString, [req.params.id], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: "der opstod en fejl ved forespørgslen!" });
+        } else {
+            // console.log(results);
+            res.json(results);
+        }
+    });
+});
+
+// ===== CREATE NEW SUBSTITUTE ===== \\
+substitutesRouter.post("/", (req, res) => {
+    let queryString = ``;
+
+    queryString = /*sql*/ `
+        INSERT INTO substitutes (FirstName, LastName, DateOfBirth, Mail, Number, IsAdmin, Username, PasswordHash) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    connection.query(
+        queryString,
+        [
+            req.body.FirstName,
+            req.body.LastName,
+            req.body.DateOfBirth,
+            req.body.Mail,
+            req.body.Number,
+            req.body.IsAdmin,
+            req.body.Username,
+            req.body.PasswordHash,
+        ],
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: "der opstod en fejl ved forespørgslen!" });
+            } else {
+                console.log(results);
+                res.json(results);
+            }
+        }
+    );
 });
 
 export default substitutesRouter;
