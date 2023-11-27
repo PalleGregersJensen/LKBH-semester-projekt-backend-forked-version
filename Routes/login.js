b74e6ed05469f8f4a0da8d52ff6a3d72ea989157
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-11-27 15:14:45 +0100

    Login-funktionalitet virker vist fornuftigt. Fejlmeddelelser skal implementeres.

--- Routes/login.js
+++ Routes/login.js
@@ -1,6 +1,7 @@
 // Importer nødvendige moduler
 import { Router } from "express";
 import connection from "../database.js";
+// import crypto to decrypt Passwordhash
 import crypto from "crypto";
 
 // Opret en router
@@ -17,12 +18,9 @@ loginRouter.post("/", (req, res) => {
     `;
 
     connection.query(queryString, [user.userName], (err, results) => {
-        if (err) {
-            console.log(err);
-            res.status(500).json({ error: "Der opstod en fejl ved forespørgslen!" });
-        } else {
+        try {
             if (results.length > 0) {
-                // Tjek om det indtastede password matcher det gemte klartekst password
+                // Tjek om det indtastede password matcher det gemte password
                 const storedPassword = results[0].PasswordHash;
                 const enteredPassword = user.password;
 
@@ -30,17 +28,25 @@ loginRouter.post("/", (req, res) => {
                 const enteredPasswordHash = crypto.createHash("sha256").update(enteredPassword).digest("hex");
 
                 if (storedPassword === enteredPasswordHash) {
+                    const employee = results[0];
                     const isAdmin = results[0].IsAdmin;
                     console.log(isAdmin);
-                    // Brugernavn og adgangskode er korrekte
+                    // Correct username and password
                     console.log("Login successful");
-                    res.json({ success: true, isAdmin });
+                    res.json({ success: true, employee });
+                } else {
+                    // Wrong username or password
+                    console.log("Login failed");
+                    res.status(401).json({ error: "Forkert brugernavn eller adgangskode!" });
                 }
             } else {
-                // Brugernavn eller adgangskode er forkert
+                // Wrong username or password
                 console.log("Login failed");
                 res.status(401).json({ error: "Forkert brugernavn eller adgangskode!" });
             }
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ error: "Der opstod en fejl ved forespørgslen!" });
         }
     });
 });
