cbb138b153d080767e9de18d68e824c560054bbb
MarkusIngerslev <markusolsen2004@gmail.com>
2023-11-28 13:13:24 +0100

    Tilføjet så det nu er muligt at oprette en ny "shift".

--- routes/shifts.js
+++ routes/shifts.js
@@ -25,6 +25,7 @@ shiftsRouter.get("/", (req, res) => {
 });
 
 // ===== GET SINGLE SHIFT WITH ID ===== \\
+// TO DO: FIX THE res.json(results) SO IT SHOWS SOMETHING USEFUL
 shiftsRouter.get("/:id", (req, res) => {
     let queryString = ``;
 
@@ -44,10 +45,38 @@ shiftsRouter.get("/:id", (req, res) => {
 });
 
 // ===== CREATE NEW SHIFT ===== \\
+// TO DO: FIX THE res.json(results) SO IT SHOWS SOMETHING USEFUL
+shiftsRouter.post("/", (req, res) => {
+    let queryString = ``;
+
+    // Formatér tidspunkterne til MySQL-format
+    const formattedShiftStart = new Date(req.body.ShiftStart).toISOString().slice(0, 19).replace("T", " ");
+    const formattedShiftEnd = new Date(req.body.ShiftEnd).toISOString().slice(0, 19).replace("T", " ");
+
+    queryString = /*sql*/ `
+        INSERT INTO shifts (Date, ShiftStart, ShiftEnd, ShiftIsTaken, EmployeeID) VALUES (?, ?, ?, ?, ?)
+    `;
+
+    connection.query(
+        queryString,
+        [req.body.Date, formattedShiftStart, formattedShiftEnd, req.body.ShiftIsTaken, req.body.EmployeeID],
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ error: "der opstod en fejl ved forespørgslen!" });
+            } else {
+                // console.log(results);
+                res.json(results);
+            }
+        }
+    );
+});
 
 // ===== UPDATE SHIFT WITH ID ===== \\
+// TO DO: FIX THE res.json(results) SO IT SHOWS SOMETHING USEFUL
 
 // ===== DELETE SHIFT WITH ID ===== \\
+// TO DO: FIX THE res.json(results) SO IT SHOWS SOMETHING USEFUL
 shiftsRouter.delete("/:id", (req, res) => {
     let queryString = ``;
 
